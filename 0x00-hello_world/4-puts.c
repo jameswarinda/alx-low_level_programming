7435265500723e29d0ab1b645b5e022af014ae07
jameswarinda <jameswarinda2@gmail.com>
2022-09-12 07:08:55 -0700

    4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 
 /**
-
-* Main - print the string in the put function
-
-* Description using the main function 
-* This program prints "Programming is like building a multilingual puzzle"
-* Return 0
-
-*/
-
+ * main - Printing
+ *
+ * Return: Always 0 (Succesfull)
+ */
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0
+	return (0)
 }
